2f6443f27c5f4e09289df6d4c10e2d86de3166c2
Arpan Samanta <117585037+ArpanSamanta2003@users.noreply.github.com>
2022-12-04 02:40:35 +0530

    Add files via upload

---
+++ calender.py
@@ -0,0 +1,9 @@
+
+import calendar
+
+yy = int(input("Enter the year(like : 2010) : "))  # year
+mm = int(input("Enter the number of month(like : 11) : ")) #month
+# display the calender
+print(calendar.month(yy,mm))
+
+
---
+++ dice_roll.py
@@ -0,0 +1,16 @@
+
+import random
+min = 1 
+max = 6
+
+roll_again = "yes"
+
+while roll_again =="yes" or roll_again =="y":
+    print("Rolling the dices...")
+    print("the values are...")
+    print(random.randint(min,max))
+    print(random.randint(min,max))
+
+    roll_again = input("roll the dices again ? : ")
+
+    
